cc0f1eea3cbbd68822333bcfcb8ea0b14491b769
Lorenz Ciocon <lorenzciocon@gmail.com>
2025-09-02 21:32:26 +0800

    Character Controller

---
+++ 3d Controller/Assets/Liliput/FinalCharacterController/Scripts/PlayerLocomotionInput.cs
@@ -0,0 +1,41 @@
+using Liliput.FinalChracterController;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+namespace Liliput.FinalCharacterController
+{
+    [DefaultExecutionOrder(-2)]
+    public class PlayerLocomotionInput : MonoBehaviour, InputSystem_Actions.IPlayerActions
+    {
+        public InputSystem_Actions InputActions { get; private set; }
+        public Vector2 MovementInput { get; private set; }
+        public Vector2 LookInput { get; private set; }
+
+        private void OnEnable()
+        {
+            InputActions = new InputSystem_Actions();
+            InputActions.Enable();
+
+            InputActions.Player.Enable();
+            InputActions.Player.SetCallbacks(this);
+        }
+
+        private void OnDisable()
+        {
+            InputActions.Player.Disable();
+            InputActions.Player.RemoveCallbacks(this);
+        }
+
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            MovementInput = context.ReadValue<Vector2>();
+        }
+
+        public void OnLook(InputAction.CallbackContext context)
+        {
+            LookInput = context.ReadValue<Vector2>();
+        }
+    }
+}
+
